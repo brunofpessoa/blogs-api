c6c857f23977a6da5d258bc68458d99a5b7fa7a3
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-10-26 19:21:24 -0300

    Cria model BlogPost e cria associação com User

---
+++ src/models/BlogPost.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+    published: {
+      type: DataTypes.DATE,
+    },
+    updated: {
+      type: DataTypes.DATE,
+    }
+  }, {
+    underscored: true,
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      as: 'user',
+      foreignKey: 'id',
+    });
+  };
+
+  return BlogPost;
+};
